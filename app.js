285172ac71a083c70fa51e0ce2cf805160ba72bc
ミンミンゼミ <24hs113@s.bgu.ac.jp>
2024-12-02 14:26:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ function checkFortune() {
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "もう一度やってね！"; // デフォルトの値
             
-            if (colour === "阪神タイガース" && number ===1) {
+            if (name === "阪神タイガース" && number ===1) {
                 result = "00	ゲラ	1995.09.25	183	86	右	左  <br>";
-            } else if (colour === "阪神タイガース" && number === 2) {
-                result = "中吉";
-            } else if (colour === "阪神タイガース" && number === 3) {
+            } else if (name === "阪神タイガース" && number === 2) {
+                result = "2	梅野　隆太郎	1991.06.17	173	79	右	右　<br>	";
+            } else if (name === "阪神タイガース" && number === 3) {
                 result = "凶";
-            } else if (colour === "阪神タイガース" && number === 4) {
+            } else if (name === "阪神タイガース" && number === 4) {
                 result = "中吉";
-            } else if (colour === "阪神タイガース" && number === 5) {
+            } else if (name === "阪神タイガース" && number === 5) {
                 result = "中吉";
-            } else if (colour === "阪神タイガース" && number === 6) {
+            } else if (name === "阪神タイガース" && number === 6) {
                 result = "大凶";
-            } else if (colour === "阪神タイガース" && number === 7) {
+            } else if (name === "阪神タイガース" && number === 7) {
                 result = "凶";
-            } else if (colour === "阪神タイガース" && number === 8) {
+            } else if (name === "阪神タイガース" && number === 8) {
                 result = "大吉";
-            } else if (colour === "阪神タイガース" && number === 9) {
+            } else if (name === "阪神タイガース" && number === 9) {
                 result = "中吉";
             }
 
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+            document.getElementById('result-output').innerText = "このポジションの選手は【" + result + "】です";
         }
