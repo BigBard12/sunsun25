c659966062983d596e848d73a18a62a8e522e665
ミンミンゼミ <24hs113@s.bgu.ac.jp>
2024-12-16 14:34:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function checkFortune() {
             
             if(position === "投手" && league ==="セリーグ") {
                result = "18　読売ジャイアンツ　菅野　智之（すがの・ともゆき）ー4年ぶり5度目"
-                            <img src="018_41745137.jpg"alt="菅野"width="40%">;
+                            img src="018_41745137.jpg"alt="菅野"width="40%";
             } else if (position === "投手" && league === "パリーグ") {
                result = "35 福岡ソフトバンクホークス Ｌ．モイネロ（LIVAN MOINELO）ー初受賞";
             }else if (position === "捕手" && league === "セリーグ") {
