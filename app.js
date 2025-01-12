bb567b93732099182841b0753826067a22f000e8
ミンミンゼミ <24hs113@s.bgu.ac.jp>
2025-01-12 21:47:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -42,6 +42,77 @@ function checkFortune() {
                result = "66　北海道日本ハムファイターズ　万波　中正（まんなみ・ちゅうせい）－2年連続2度目";
             } 
 
-
-            document.getElementById('result-output').innerText = "このポジションの受賞選手は【" + result + "】です";
+             // 結果を表示
+            document.getElementById('result-output').innerText = `このポジションの受賞選手は【${result}】です`;
         }
+
+             // 画像の切り替え
+             const imageContainer = document.getElementById('image-container');
+             imageContainer.innerHTML = ""; // 既存の画像をクリア
+
+    const img = document.createElement('img');
+    switch (result) {
+        case "18　読売ジャイアンツ　菅野　智之（すがの・ともゆき）ー4年ぶり5度目":
+            img.src = "images/018_41745137.jpg";
+            "セリーグ";
+        case "50　横浜DeNAベイスターズ　山本　祐大（やまもと・ゆうだい）ー初受賞":
+            img.src = "images/050_23125136.jpg";
+            "セリーグ";
+　　　　　case "25　読売ジャイアンツ　岡本　和真（おかもと・かずま）ー初受賞（他に三塁手で2度）":
+            img.src = "images/025_11515130.jpg";
+            "セリーグ";
+　　　　　case "2　読売ジャイアンツ　吉川　尚輝（よしかわ・なおき）ー初受賞":
+            img.src = "images/002_73375134.jpg";
+            "セリーグ";
+　　　　　case "6　読売ジャイアンツ　坂本　勇人（さかもと・はやと）ー初受賞（他に遊撃手で5度）":
+            img.src = "images/006_51955114.jpg";
+            "セリーグ";
+       case "61　広島東洋カープ　矢野　雅哉（やの・まさや）ー初受賞":
+            img.src = "images/ 061_93395153.jpg";
+            "セリーグ";
+　　　　　case "9 広島東洋カープ 秋山　翔吾（あきやま・しょうご）ー初受賞（他にパ外野手で6度）":
+            img.src = "images/009_31135133.jpg";
+            "セリーグ";
+        case "1　中日ドラゴンズ　岡林　勇希（おかばやし・ゆうき）ー3年連続3度目":
+            img.src = "images/001_81585151.jpg";
+            "セリーグ";
+        default:"5　阪神タイガース　近本　光司（ちかもと・こうじ）ー4年連続4度目":
+            img.src = "images/005_71075138.jpg";
+            "セリーグ";
+    }
+
+ switch (result) {
+        case "35 福岡ソフトバンクホークス Ｌ．モイネロ（LIVAN MOINELO）ー初受賞":
+            img.src = "images/035_63965134.jpg";
+            "パリーグ";
+        case "19　福岡ソフトバンクホークス　甲斐　拓也（かい・たくや）ー2年ぶり7度目":
+            img.src = "images/019_91595133.jpg";
+            "パリーグ";
+　　　　　case "25　福岡ソフトバンクホークス　山川　穂高（やまかわ・ほたか）ー初受賞":
+            img.src = "images/025_21425139.jpg";
+            "パリーグ";
+　　　　　case "0　東北楽天ゴールデンイーグルス　小深田　大翔（こぶかた・ひろと）ー初受賞":
+            img.src = "images/000_61965151.jpg";
+            "パリーグ";
+　　　　　case "24　福岡ソフトバンクホークス　栗原　陵矢（くりはら・りょうや）ー初受賞":
+            img.src = "images/024_01705130.jpg";
+            "パリーグ";
+       case "6　埼玉西武ライオンズ　源田　壮亮（げんだ・そうすけ）ー7連続7度目":
+            img.src = "images/006_71775134.jpg";
+            "パリーグ";
+　　　　　case "8　東北楽天ゴールデンイーグルス　辰己　涼介（たつみ・りょうすけ）ー4年連続4度目":
+            img.src = "images/008_11015138.jpg";
+            "パリーグ";
+        case "23　福岡ソフトバンクホークス　周東　佑京（しゅうとう・うきょう）ー初受賞":
+            img.src = "images/023_21925136.jpg";
+            "パリーグ";
+        default:"66　北海道日本ハムファイターズ　万波　中正（まんなみ・ちゅうせい）－2年連続2度目":
+            img.src = "images/066_11815138.jpg";
+            "パリーグ";
+    }
+
+
+ img.alt = result; // アクセシビリティのためのalt属性
+    img.style.width = "300px"; // 必要に応じてスタイルを調整
+    imageContainer.appendChild(img);
+}
